f764c6e2138455b2d76aa80e794615bd294173e8
Ronak <ronaklulla26@outlook.com>
2022-09-10 12:35:07 -0700

    CSHS

--- helloworld/express-locallibrary-tutorial/app.js
+++ helloworld/express-locallibrary-tutorial/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var wikiRouter = require('./routes/wiki') //ADD THE WIKI ROUTER TO THE APP
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/wiki', wikiRouter); //USE THE WIKI ROUTER
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
